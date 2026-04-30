e315cc00e452fdce92a8c777c43987bb53569ab1
Mengheng855 <mengheng.mh855@gmail.com>
2026-04-30 16:33:24 +0700

    test_hosting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Storage;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,12 +16,19 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
+    
+    
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+
+    public function boot()
     {
-        //
+        if (app()->environment('production') || env('VERCEL')) {
+            URL::forceScheme('https');
+
+            @mkdir('/tmp/storage/framework/views', 0777, true);
+            @mkdir('/tmp/storage/framework/cache', 0777, true);
+        }
     }
 }
